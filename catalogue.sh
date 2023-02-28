7d01bdab05b2a20ffde3fd795a0ebc0e5267f112
manisha guda <manishavguda@gmail.com>
2023-02-28 08:14:26 +0530

    catalogue Script

--- catalogue.sh
+++ catalogue.sh
@@ -1,11 +1,9 @@
 script_location=$(pwd)
 
-set -e
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-#useradd roboshop
-
-mkdir /app
+useradd roboshop
+mkdir -p /app
 
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
@@ -16,6 +14,10 @@ npm install
 
  cp ${script_location}/files/catalogue.service/etc/sustemd/system/catalogue.service
  systemctl daemon-reload
-
 systemctl enable catalogue
 systemctl start catalogue
+
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+yum install mongodb-org-shell -y
+
+mongo --host localhost </app/schema/catalogue.js
