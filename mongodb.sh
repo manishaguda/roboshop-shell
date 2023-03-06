539f50e205e1a0bf6fa2b13b15e240443b1ab995
manisha guda <manishavguda@gmail.com>
2023-03-06 17:59:43 +0530

    Creat Server Script

--- mongodb.sh
+++ mongodb.sh
@@ -5,7 +5,7 @@ cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org -y
 
 
-sed -e 's127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -e '127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 systemctl enable mongod
 systemctl start mongod
