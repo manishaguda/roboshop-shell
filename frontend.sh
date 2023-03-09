564bb2c4470344071423f0f62b07394c43227592
manisha guda <manishavguda@gmail.com>
2023-03-09 17:43:17 +0530

    Create Server Script

--- frontend.sh
+++ frontend.sh
@@ -19,23 +19,43 @@ fi
 
 echo -e "\e[31m Download Frontend Contend\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
-echo $?
+echo [ $? -eq 0 ] then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 cd /usr/share/nginx/html &>>${LOG}
 
 echo -e "\e[31m Extract frontend content\e[0m"
 unzip /tmp/frontend.zip &>>${LOG}
-echo $?
+echo [ $? -eq 0 ] then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 echo -e "\e[31m Copy Roboshop Nginx Config File\e0m"
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
-echo $?
+echo [ $? -eq 0 ] then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 echo -e "\e[31m Enable Nginx\e[0m"
 systemctl enable nginx &>>${LOG}
-echo $?
+echo [ $? -eq 0 ] then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 echo -e "\e[31m Restart Nginx\e[0m"
 systemctl restart nginx &>>${LOG}
-echo $?
+echo [ $? -eq 0 ] then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
