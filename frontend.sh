8cd15bae9a077cba4342c782b652e7a59305a899
manisha guda <manishavguda@gmail.com>
2023-03-09 17:15:37 +0530

    Create Server Script

--- frontend.sh
+++ frontend.sh
@@ -3,26 +3,31 @@ LOG=tmp/roboshop.log
 
 echo -e "\e[31m Install Nginx\e[0m"
 yum install nginx -y &>>${LOG}
-
+echo $?
 
 echo -e "\e[31m Remove Nginx Old Content\e[0m"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
+echo $?
 
 echo -e "\e[31m Download Frontend Contend\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
+echo $?
 
 cd /usr/share/nginx/html &>>${LOG}
 
 echo -e "\e[31m Extract frontend content\e[0m"
 unzip /tmp/frontend.zip &>>${LOG}
+echo $?
 
 echo -e "\e[31m Copy Roboshop Nginx Config File \e0m"
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
+echo $?
 
 echo -e "\e[31m Enable Nginx\e[0m"
 systemctl enable nginx &>>${LOG}
+echo $?
 
 echo -e "\e[31m Restart Nginx \e[0m"
 systemctl restart nginx &>>${LOG}
-
+echo $?
 
