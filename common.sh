9770b293cbf07c83dbcd6843e5c7e33d24b4e9be
manisha guda <manishavguda@gmail.com>
2023-03-10 22:12:11 +0530

    Create Server Script

--- common.sh
+++ common.sh
@@ -1,20 +1,24 @@
 script_location=$(pwd)
 LOG=/tmp/roboshop.log
 
-  status_check() {
+status_check() {
    if [ $? -eq 0 ]; then
      echo -e "\e[1;32mSUCCESS\e[0m"
    else
      echo -e "\e[1;31mFAILURE\e[0m"
      echo "Refer Log file more information, LOG - ${LOG}"
-     exit
+     exit 1
     fi
  }
 
 print_head() {
   echo -e "\e[1m $1 \e[0m"
+
  }
+
 APP_PREREQ() {
+
+
     print_head "Add Application User"
     id roboshop &>>${LOG}
     if [ $? -ne 0 ]; then
