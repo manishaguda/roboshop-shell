716a7d8cba60202d5b7509b3f0cae192541473ca
manisha guda <manishavguda@gmail.com>
2023-03-10 22:19:42 +0530

    Create Server Script

--- shipping.sh
+++ shipping.sh
@@ -1,10 +1,9 @@
 source common.sh
 
-if [ -z "${root_mysql_password}  ]; then
- echo "variable root_mysql_password is needed"
- exit
+if [ -z "${root_mysql_password}"  ]; then
+    echo "variable root_mysql_password is needed"
+    exit
 fi
-
 
 component=shipping
 schema_load=true
