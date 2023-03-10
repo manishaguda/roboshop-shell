086e5e59fc1135439620556cb57640878aa0568d
manisha guda <manishavguda@gmail.com>
2023-03-10 22:09:26 +0530

    Create Server Script

--- common.sh
+++ common.sh
@@ -1,13 +1,13 @@
 script_location=$(pwd)
 LOG=/tmp/roboshop.log
 
-status_check() {
-  if [ $? -eq 0 ]; then
-    echo -e "\e[1;32mSUCCESS\e[0m"
-  else
-    echo -e "\e[1;31mFAILURE\e[0m"
-    echo "Refer Log file more information, LOG - ${LOG}"
-    exit
+  status_check() {
+   if [ $? -eq 0 ]; then
+     echo -e "\e[1;32mSUCCESS\e[0m"
+   else
+     echo -e "\e[1;31mFAILURE\e[0m"
+     echo "Refer Log file more information, LOG - ${LOG}"
+     exit
     fi
  }
 
