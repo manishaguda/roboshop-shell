03c7474778248b7c4a339f298178f42ac5bc8ef1
manisha guda <manishavguda@gmail.com>
2023-03-10 21:56:12 +0530

    Create Server Script

--- catalogue.sh
+++ catalogue.sh
@@ -2,6 +2,7 @@ source common.sh
 
 component=catalogue
 schema_load=true
+schema_type=mongo
 NODEJS
 
 
--- common.sh
+++ common.sh
@@ -12,73 +12,123 @@ status_check() {
 }
 
 print_head() {
-  echo -e "\e[1m $1  \e[0m"
+  echo -e "\e[1m $1 \e[0m"
+}
+APP_PREREQ() {
+    print_head "Add Application User"
+    id roboshop &>>${LOG}
+    if [ $? -ne 0 ]; then
+      useradd roboshop &>>${LOG}
+      fi
+    status_check
+
+    mkdir -p /app &>>${LOG}
+
+       print_head "Downloading App content"
+      curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip&>>${LOG}
+      status_check
+
+      print_head "Cleanup Old Content"
+      rm -rf /app/* &>>${LOG}
+      status_check
+
+      print_head "Extracting App Content"
+      cd /app
+      unzip /tmp/${component}.zip &>>${LOG}
+      status_check
+
+
 }
 
-NODEJS() {
-  print_head "Configure nodeJS repos"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
-  status_check
+SYSTEMD_SETUP() {
+      print_head "Configuring ${component} Service File"
+      cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
+      status_check
 
-  print_head "Install NodeJS"
-  echo -e "\e[31m \e[0m"
-  yum install nodejs -y &>>${LOG}
-  status_check
+      print_head "Reload SystemD"
+      systemctl daemon-reload &>>${LOG}
+      status_check
 
-  print_head "Add Application User"
-  id roboshop &>>${LOG}
-  if [ $? -ne 0 ]; then
-    useradd roboshop &>>${LOG}
+      print_head "Enable ${component} Service"
+      systemctl enable ${component} &>>${LOG}
+      status_check
+
+      print_head "Start ${component} Service"
+      systemctl start ${component} &>>${LOG}
+      status_check
+}
+
+LOAD_SCHEMA() {
+    if [ ${schema_load} == "true" ]; then
+
+      if [ ${schema_type} == "mongo" ]; then
+        print_head "configuring Mongo Repo "
+        cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
+        status_check
+
+        print_head "Install Mongo Repo"
+       yum install mongodb-org-shell -y &>>${LOG}
+       status_check
+
+       print_head "Load Schema "
+       mongo --host mongodb-dev.manishag.online </app/schema/${component}.js &>>${LOG}
+       status_check
+      fi
+
+      if [ ${schema_type} == "mysql" ]; then
+
+        print_head "Install MySQl Client"
+        yum install mysql -y &>>${LOG}
+        status_check
+
+        print_head "Load Schema "
+              mysql -h mysql-dev.manishag.online -uroot -p${root_mysql_password} < /app/schema/shipping.sql
+              status_check
+             fi
     fi
+}
+
+NODEJS() {print_head "Configuring NodeJS Repos"
+            curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+            status_check
+
+        print_head "Install NodeJS"
+        yum install nodejs -y &>>${LOG}
+        status_check
+
+         APP_PREREQ
+
+         print_head "Installing NodeJS Dependencies"
+         cd /app &>>${LOG}
+         npm install &>>${LOG}
+         status_check
+
+         SYSTEMD_SETUP
+
+         LOAD_SCHEMA
+
+
+
+}
+
+MAVEN() {
+
+  print_head "Install Maven"
+  echo -e "\e[31m \e[0m"
+  yum install maven -y &>>${LOG}
+  status_check
+  APP_PREREQ
+
+  print_head "Build a package"
+  mvn clean package
+
   status_check
 
-  print_head "Downloading App content"
-  mkdir -p /app &>>${LOG}
-
-  print_head "Cleanup Old Content"
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip&>>${LOG}
+  print_head "Copy App File To App Location"
+  mv target/${component}g-1.0.jar ${component}.jar &>>${LOG}
   status_check
 
-  print_head "Extracting App Content"
-  rm -rf /app/* &>>${LOG}
-  status_check
+  SYSTEMD_SETUP
 
-  print_head "Installing NodeJS Dependencies"
-  cd /app
-  unzip /tmp/${component}.zip &>>${LOG}
-
-  print_head "Configure User Service File"
-  cd /app &>>${LOG}
-  npm install &>>${LOG}
-  status_check
-
-  print_head "Configuring User Service File"
-  cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
-  status_check
-
-  print_head "Reload SystemD"
-  systemctl daemon-reload &>>${LOG}
-  status_check
-
-  print_head "Enable ${component} Service"
-  systemctl enable ${component} &>>${LOG}
-  status_check
-
-  print_head "Start ${component} Service"
-  systemctl start ${component} &>>${LOG}
-  status_check
-
-  if [ ${schema_load} == "true" ]; then
-  print_head "configuring Mongo Repo "
-  cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
-  status_check
-
-  print_head "Install Mongo Repo"
-  yum install mongodb-org-shell -y &>>${LOG}
-  status_check
-
-  print_head "Load Schema "
-  mongo --host mongodb-dev.manishag.online </app/schema/${component}.js &>>${LOG}
-  status_check
-  fi
+  LOAD_SCHEMA
 }
