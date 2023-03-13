3098226da31a395d78279148d64e2ac022b06209
manisha guda <manishavguda@gmail.com>
2023-03-13 12:06:39 +0530

    Create Server Script

--- payment.sh
+++ payment.sh
@@ -4,7 +4,7 @@ component=payment
 schema_load=false
 
 if [ -z "${roboshop_rabbitmq_password}" ]; then
-  echo "variable root_rabbitmq_password is missing"
+  echo "Variable roboshop_rabbitmq_password is missing"
   exit 1
  fi
 
