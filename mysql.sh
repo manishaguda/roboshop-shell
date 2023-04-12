4696ecf6ea6dcae8eaaff64546855340831aad8e
manisha guda <manishavguda@gmail.com>
2023-04-12 09:49:55 +0530

    Create Server Script

--- mysql.sh
+++ mysql.sh
@@ -1,30 +1,34 @@
 source common.sh
 
 if [ -z "${root_mysql_password}" ]; then
-  echo "variable root_mysql_password is missing"
-  exit
-  fi
+  echo "Variable root_mysql_password is missing"
+  exit 1
+fi
 
-print_head "Disable MySql Default Module"
+
+print_head "Disable MySQL Default Module"
 dnf module disable mysql -y &>>${LOG}
 status_check
 
-print_head "Copy MySql Repo file"
+print_head "Copy MySQL Repo file"
 cp ${script_location}/files/mysql.repo /etc/yum.repos.d/mysql.repo &>>${LOG}
 status_check
 
-print_head "Install MySql Server"
-yum install mysql-community-server -y &>>${LOG}
+print_head "Install MySQL Server"
+yum install mysql-community-server -y  &>>${LOG}
 status_check
 
-print_head "Enable MySql"
+print_head "Enable MySQL"
 systemctl enable mysqld &>>${LOG}
 status_check
 
-print_head "Start MySql"
+print_head "Start MySQL"
 systemctl restart mysqld &>>${LOG}
 status_check
 
 print_head "Reset Default Database Password"
 mysql_secure_installation --set-root-pass ${root_mysql_password} &>>${LOG}
+if [ $? -eq 1 ]; then
+  echo "Password is already changed"
+fi
 status_check
